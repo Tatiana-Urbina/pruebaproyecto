5450ed0cf312276abbf6b30f8a551323821892df
Urbina Tatiana <urbinatatiana077@gmail.com>
2021-07-25 10:37:38 -0300

    subiendo los primeros archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './inicio/body/body.component';
+import { FooterComponent } from './inicio/footer/footer.component';
+import { NavbarComponent } from './inicio/navbar/navbar.component';
+import { LoginComponent } from './inicio/login/login.component';
+import { ReactiveFormsModule }from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/inicio/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators,} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+email=new FormControl ('', [Validators.required,Validators.email],[])
+password= new FormControl ('',[])
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // ...
+  }
+  }
